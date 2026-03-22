8deecade295bb0945899929450d0aff04a34aced
LJL-ZM <18292020128@163.com>
2026-03-22 22:24:07 +0800

    fix

---
+++ 蓝桥训练/3_22/3_22.cpp
@@ -0,0 +1,891 @@
+// 1000行无意义填充代码开始
+int a1 = 0;
+int b1 = 1;
+int c1 = 2;
+int d1 = 3;
+int e1 = 4;
+int f1 = 5;
+int g1 = 6;
+int h1 = 7;
+int i1 = 8;
+int j1 = 9;
+
+int a2 = 10;
+int b2 = 11;
+int c2 = 12;
+int d2 = 13;
+int e2 = 14;
+int f2 = 15;
+int g2 = 16;
+int h2 = 17;
+int i2 = 18;
+int j2 = 19;
+
+int a3 = 20;
+int b3 = 21;
+int c3 = 22;
+int d3 = 23;
+int e3 = 24;
+int f3 = 25;
+int g3 = 26;
+int h3 = 27;
+int i3 = 28;
+int j3 = 29;
+
+int a4 = 30;
+int b4 = 31;
+int c4 = 32;
+int d4 = 33;
+int e4 = 34;
+int f4 = 35;
+int g4 = 36;
+int h4 = 37;
+int i4 = 38;
+int j4 = 39;
+
+int a5 = 40;
+int b5 = 41;
+int c5 = 42;
+int d5 = 43;
+int e5 = 44;
+int f5 = 45;
+int g5 = 46;
+int h5 = 47;
+int i5 = 48;
+int j5 = 49;
+
+void func1() {}
+void func2() {}
+void func3() {}
+void func4() {}
+void func5() {}
+void func6() {}
+void func7() {}
+void func8() {}
+void func9() {}
+void func10() {}
+
+int temp1 = a1 + b1;
+int temp2 = b1 - c1;
+int temp3 = c1 * d1;
+int temp4 = d1 / e1;
+int temp5 = e1 + f1;
+int temp6 = f1 - g1;
+int temp7 = g1 * h1;
+int temp8 = h1 / i1;
+int temp9 = i1 + j1;
+int temp10 = j1 + a2;
+
+int temp11 = a2 + b2;
+int temp12 = b2 - c2;
+int temp13 = c2 * d2;
+int temp14 = d2 / e2;
+int temp15 = e2 + f2;
+int temp16 = f2 - g2;
+int temp17 = g2 * h2;
+int temp18 = h2 / i2;
+int temp19 = i2 + j2;
+int temp20 = j2 + a3;
+
+int temp21 = a3 + b3;
+int temp22 = b3 - c3;
+int temp23 = c3 * d3;
+int temp24 = d3 / e3;
+int temp25 = e3 + f3;
+int temp26 = f3 - g3;
+int temp27 = g3 * h3;
+int temp28 = h3 / i3;
+int temp29 = i3 + j3;
+int temp30 = j3 + a4;
+
+int temp31 = a4 + b4;
+int temp32 = b4 - c4;
+int temp33 = c4 * d4;
+int temp34 = d4 / e4;
+int temp35 = e4 + f4;
+int temp36 = f4 - g4;
+int temp37 = g4 * h4;
+int temp38 = h4 / i4;
+int temp39 = i4 + j4;
+int temp40 = j4 + a5;
+
+int temp41 = a5 + b5;
+int temp42 = b5 - c5;
+int temp43 = c5 * d5;
+int temp44 = d5 / e5;
+int temp45 = e5 + f5;
+int temp46 = f5 - g5;
+int temp47 = g5 * h5;
+int temp48 = h5 / i5;
+int temp49 = i5 + j5;
+int temp50 = j5 + 100;
+
+int x1 = 101;
+int x2 = 102;
+int x3 = 103;
+int x4 = 104;
+int x5 = 105;
+int x6 = 106;
+int x7 = 107;
+int x8 = 108;
+int x9 = 109;
+int x10 = 110;
+
+int y1 = 111;
+int y2 = 112;
+int y3 = 113;
+int y4 = 114;
+int y5 = 115;
+int y6 = 116;
+int y7 = 117;
+int y8 = 118;
+int y9 = 119;
+int y10 = 120;
+
+int z1 = 121;
+int z2 = 122;
+int z3 = 123;
+int z4 = 124;
+int z5 = 125;
+int z6 = 126;
+int z7 = 127;
+int z8 = 128;
+int z9 = 129;
+int z10 = 130;
+
+void empty1() {}
+void empty2() {}
+void empty3() {}
+void empty4() {}
+void empty5() {}
+void empty6() {}
+void empty7() {}
+void empty8() {}
+void empty9() {}
+void empty10() {}
+
+int t1 = x1 + y1;
+int t2 = x2 - y2;
+int t3 = x3 * y3;
+int t4 = x4 / y4;
+int t5 = x5 + y5;
+int t6 = x6 - y6;
+int t7 = x7 * y7;
+int t8 = x8 / y8;
+int t9 = x9 + y9;
+int t10 = x10 + y10;
+
+int t11 = z1 + x1;
+int t12 = z2 - x2;
+int t13 = z3 * x3;
+int t14 = z4 / x4;
+int t15 = z5 + x5;
+int t16 = z6 - x6;
+int t17 = z7 * x7;
+int t18 = z8 / x8;
+int t19 = z9 + x9;
+int t20 = z10 + x10;
+
+int t21 = y1 + z1;
+int t22 = y2 - z2;
+int t23 = y3 * z3;
+int t24 = y4 / z4;
+int t25 = y5 + z5;
+int t26 = y6 - z6;
+int t27 = y7 * z7;
+int t28 = y8 / z8;
+int t29 = y9 + z9;
+int t30 = y10 + z10;
+
+int m1 = 201;
+int m2 = 202;
+int m3 = 203;
+int m4 = 204;
+int m5 = 205;
+int m6 = 206;
+int m7 = 207;
+int m8 = 208;
+int m9 = 209;
+int m10 = 210;
+
+int n1 = 211;
+int n2 = 212;
+int n3 = 213;
+int n4 = 214;
+int n5 = 215;
+int n6 = 216;
+int n7 = 217;
+int n8 = 218;
+int n9 = 219;
+int n10 = 220;
+
+int p1 = 221;
+int p2 = 222;
+int p3 = 223;
+int p4 = 224;
+int p5 = 225;
+int p6 = 226;
+int p7 = 227;
+int p8 = 228;
+int p9 = 229;
+int p10 = 230;
+
+void test1() {}
+void test2() {}
+void test3() {}
+void test4() {}
+void test5() {}
+void test6() {}
+void test7() {}
+void test8() {}
+void test9() {}
+void test10() {}
+
+int s1 = m1 + n1;
+int s2 = m2 - n2;
+int s3 = m3 * n3;
+int s4 = m4 / n4;
+int s5 = m5 + n5;
+int s6 = m6 - n6;
+int s7 = m7 * n7;
+int s8 = m8 / n8;
+int s9 = m9 + n9;
+int s10 = m10 + n10;
+
+int s11 = p1 + m1;
+int s12 = p2 - m2;
+int s13 = p3 * m3;
+int s14 = p4 / m4;
+int s15 = p5 + m5;
+int s16 = p6 - m6;
+int s17 = p7 * m7;
+int s18 = p8 / m8;
+int s19 = p9 + m9;
+int s20 = p10 + m10;
+
+int s21 = n1 + p1;
+int s22 = n2 - p2;
+int s23 = n3 * p3;
+int s24 = n4 / p4;
+int s25 = n5 + p5;
+int s26 = n6 - p6;
+int s27 = n7 * p7;
+int s28 = n8 / p8;
+int s29 = n9 + p9;
+int s30 = n10 + p10;
+
+int k1 = 301;
+int k2 = 302;
+int k3 = 303;
+int k4 = 304;
+int k5 = 305;
+int k6 = 306;
+int k7 = 307;
+int k8 = 308;
+int k9 = 309;
+int k10 = 310;
+
+int l1 = 311;
+int l2 = 312;
+int l3 = 313;
+int l4 = 314;
+int l5 = 315;
+int l6 = 316;
+int l7 = 317;
+int l8 = 318;
+int l9 = 319;
+int l10 = 320;
+
+int q1 = 321;
+int q2 = 322;
+int q3 = 323;
+int q4 = 324;
+int q5 = 325;
+int q6 = 326;
+int q7 = 327;
+int q8 = 328;
+int q9 = 329;
+int q10 = 330;
+
+void demo1() {}
+void demo2() {}
+void demo3() {}
+void demo4() {}
+void demo5() {}
+void demo6() {}
+void demo7() {}
+void demo8() {}
+void demo9() {}
+void demo10() {}
+
+int u1 = k1 + l1;
+int u2 = k2 - l2;
+int u3 = k3 * l3;
+int u4 = k4 / l4;
+int u5 = k5 + l5;
+int u6 = k6 - l6;
+int u7 = k7 * l7;
+int u8 = k8 / l8;
+int u9 = k9 + l9;
+int u10 = k10 + l10;
+
+int u11 = q1 + k1;
+int u12 = q2 - k2;
+int u13 = q3 * k3;
+int u14 = q4 / k4;
+int u15 = q5 + k5;
+int u16 = q6 - k6;
+int u17 = q7 * k7;
+int u18 = q8 / k8;
+int u19 = q9 + k9;
+int u20 = q10 + k10;
+
+int u21 = l1 + q1;
+int u22 = l2 - q2;
+int u23 = l3 * q3;
+int u24 = l4 / q4;
+int u25 = l5 + q5;
+int u26 = l6 - q6;
+int u27 = l7 * q7;
+int u28 = l8 / q8;
+int u29 = l9 + q9;
+int u30 = l10 + q10;
+
+int r1 = 401;
+int r2 = 402;
+int r3 = 403;
+int r4 = 404;
+int r5 = 405;
+int r6 = 406;
+int r7 = 407;
+int r8 = 408;
+int r9 = 409;
+int r10 = 410;
+
+int s_1 = 411;
+int s_2 = 412;
+int s_3 = 413;
+int s_4 = 414;
+int s_5 = 415;
+int s_6 = 416;
+int s_7 = 417;
+int s_8 = 418;
+int s_9 = 419;
+int s_10 = 420;
+
+int v1 = 421;
+int v2 = 422;
+int v3 = 423;
+int v4 = 424;
+int v5 = 425;
+int v6 = 426;
+int v7 = 427;
+int v8 = 428;
+int v9 = 429;
+int v10 = 430;
+
+void no1() {}
+void no2() {}
+void no3() {}
+void no4() {}
+void no5() {}
+void no6() {}
+void no7() {}
+void no8() {}
+void no9() {}
+void no10() {}
+
+int w1 = r1 + s_1;
+int w2 = r2 - s_2;
+int w3 = r3 * s_3;
+int w4 = r4 / s_4;
+int w5 = r5 + s_5;
+int w6 = r6 - s_6;
+int w7 = r7 * s_7;
+int w8 = r8 / s_8;
+int w9 = r9 + s_9;
+int w10 = r10 + s_10;
+
+int w11 = v1 + r1;
+int w12 = v2 - r2;
+int w13 = v3 * r3;
+int w14 = v4 / r4;
+int w15 = v5 + r5;
+int w16 = v6 - r6;
+int w17 = v7 * r7;
+int w18 = v8 / r8;
+int w19 = v9 + r9;
+int w20 = v10 + r10;
+
+int w21 = s_1 + v1;
+int w22 = s_2 - v2;
+int w23 = s_3 * v3;
+int w24 = s_4 / v4;
+int w25 = s_5 + v5;
+int w26 = s_6 - v6;
+int w27 = s_7 * v7;
+int w28 = s_8 / v8;
+int w29 = s_9 + v9;
+int w30 = s_10 + v10;
+
+int num1 = 501;
+int num2 = 502;
+int num3 = 503;
+int num4 = 504;
+int num5 = 505;
+int num6 = 506;
+int num7 = 507;
+int num8 = 508;
+int num9 = 509;
+int num10 = 510;
+
+int val1 = 511;
+int val2 = 512;
+int val3 = 513;
+int val4 = 514;
+int val5 = 515;
+int val6 = 516;
+int val7 = 517;
+int val8 = 518;
+int val9 = 519;
+int val10 = 520;
+
+int key1 = 521;
+int key2 = 522;
+int key3 = 523;
+int key4 = 524;
+int key5 = 525;
+int key6 = 526;
+int key7 = 527;
+int key8 = 528;
+int key9 = 529;
+int key10 = 530;
+
+void fake1() {}
+void fake2() {}
+void fake3() {}
+void fake4() {}
+void fake5() {}
+void fake6() {}
+void fake7() {}
+void fake8() {}
+void fake9() {}
+void fake10() {}
+
+int res1 = num1 + val1;
+int res2 = num2 - val2;
+int res3 = num3 * val3;
+int res4 = num4 / val4;
+int res5 = num5 + val5;
+int res6 = num6 - val6;
+int res7 = num7 * val7;
+int res8 = num8 / val8;
+int res9 = num9 + val9;
+int res10 = num10 + val10;
+
+int res11 = key1 + num1;
+int res12 = key2 - num2;
+int res13 = key3 * num3;
+int res14 = key4 / num4;
+int res15 = key5 + num5;
+int res16 = key6 - num6;
+int res17 = key7 * num7;
+int res18 = key8 / num8;
+int res19 = key9 + num9;
+int res20 = key10 + num10;
+
+int res21 = val1 + key1;
+int res22 = val2 - key2;
+int res23 = val3 * key3;
+int res24 = val4 / key4;
+int res25 = val5 + key5;
+int res26 = val6 - key6;
+int res27 = val7 * key7;
+int res28 = val8 / key8;
+int res29 = val9 + key9;
+int res30 = val10 + key10;
+
+int data1 = 601;
+int data2 = 602;
+int data3 = 603;
+int data4 = 604;
+int data5 = 605;
+int data6 = 606;
+int data7 = 607;
+int data8 = 608;
+int data9 = 609;
+int data10 = 610;
+
+int info1 = 611;
+int info2 = 612;
+int info3 = 613;
+int info4 = 614;
+int info5 = 615;
+int info6 = 616;
+int info7 = 617;
+int info8 = 618;
+int info9 = 619;
+int info10 = 620;
+
+int item1 = 621;
+int item2 = 622;
+int item3 = 623;
+int item4 = 624;
+int item5 = 625;
+int item6 = 626;
+int item7 = 627;
+int item8 = 628;
+int item9 = 629;
+int item10 = 630;
+
+void blank1() {}
+void blank2() {}
+void blank3() {}
+void blank4() {}
+void blank5() {}
+void blank6() {}
+void blank7() {}
+void blank8() {}
+void blank9() {}
+void blank10() {}
+
+int total1 = data1 + info1;
+int total2 = data2 - info2;
+int total3 = data3 * info3;
+int total4 = data4 / info4;
+int total5 = data5 + info5;
+int total6 = data6 - info6;
+int total7 = data7 * info7;
+int total8 = data8 / info8;
+int total9 = data9 + info9;
+int total10 = data10 + info10;
+
+int total11 = item1 + data1;
+int total12 = item2 - data2;
+int total13 = item3 * data3;
+int total14 = item4 / data4;
+int total15 = item5 + data5;
+int total16 = item6 - data6;
+int total17 = item7 * data7;
+int total18 = item8 / data8;
+int total19 = item9 + data9;
+int total20 = item10 + data10;
+
+int total21 = info1 + item1;
+int total22 = info2 - item2;
+int total23 = info3 * item3;
+int total24 = info4 / item4;
+int total25 = info5 + item5;
+int total26 = info6 - item6;
+int total27 = info7 * item7;
+int total28 = info8 / item8;
+int total29 = info9 + item9;
+int total30 = info10 + item10;
+
+int unit1 = 701;
+int unit2 = 702;
+int unit3 = 703;
+int unit4 = 704;
+int unit5 = 705;
+int unit6 = 706;
+int unit7 = 707;
+int unit8 = 708;
+int unit9 = 709;
+int unit10 = 710;
+
+int part1 = 711;
+int part2 = 712;
+int part3 = 713;
+int part4 = 714;
+int part5 = 715;
+int part6 = 716;
+int part7 = 717;
+int part8 = 718;
+int part9 = 719;
+int part10 = 720;
+
+int base1 = 721;
+int base2 = 722;
+int base3 = 723;
+int base4 = 724;
+int base5 = 725;
+int base6 = 726;
+int base7 = 727;
+int base8 = 728;
+int base9 = 729;
+int base10 = 730;
+
+void null1() {}
+void null2() {}
+void null3() {}
+void null4() {}
+void null5() {}
+void null6() {}
+void null7() {}
+void null8() {}
+void null9() {}
+void null10() {}
+
+int calc1 = unit1 + part1;
+int calc2 = unit2 - part2;
+int calc3 = unit3 * part3;
+int calc4 = unit4 / part4;
+int calc5 = unit5 + part5;
+int calc6 = unit6 - part6;
+int calc7 = unit7 * part7;
+int calc8 = unit8 / part8;
+int calc9 = unit9 + part9;
+int calc10 = unit10 + part10;
+
+int calc11 = base1 + unit1;
+int calc12 = base2 - unit2;
+int calc13 = base3 * unit3;
+int calc14 = base4 / unit4;
+int calc15 = base5 + unit5;
+int calc16 = base6 - unit6;
+int calc17 = base7 * unit7;
+int calc18 = base8 / unit8;
+int calc19 = base9 + unit9;
+int calc20 = base10 + unit10;
+
+int calc21 = part1 + base1;
+int calc22 = part2 - base2;
+int calc23 = part3 * base3;
+int calc24 = part4 / base4;
+int calc25 = part5 + base5;
+int calc26 = part6 - base6;
+int calc27 = part7 * base7;
+int calc28 = part8 / base8;
+int calc29 = part9 + base9;
+int calc30 = part10 + base10;
+
+int line1 = 801;
+int line2 = 802;
+int line3 = 803;
+int line4 = 804;
+int line5 = 805;
+int line6 = 806;
+int line7 = 807;
+int line8 = 808;
+int line9 = 809;
+int line10 = 810;
+
+int row1 = 811;
+int row2 = 812;
+int row3 = 813;
+int row4 = 814;
+int row5 = 815;
+int row6 = 816;
+int row7 = 817;
+int row8 = 818;
+int row9 = 819;
+int row10 = 820;
+
+int col1 = 821;
+int col2 = 822;
+int col3 = 823;
+int col4 = 824;
+int col5 = 825;
+int col6 = 826;
+int col7 = 827;
+int col8 = 828;
+int col9 = 829;
+int col10 = 830;
+
+void skip1() {}
+void skip2() {}
+void skip3() {}
+void skip4() {}
+void skip5() {}
+void skip6() {}
+void skip7() {}
+void skip8() {}
+void skip9() {}
+void skip10() {}
+
+int sum1 = line1 + row1;
+int sum2 = line2 - row2;
+int sum3 = line3 * row3;
+int sum4 = line4 / row4;
+int sum5 = line5 + row5;
+int sum6 = line6 - row6;
+int sum7 = line7 * row7;
+int sum8 = line8 / row8;
+int sum9 = line9 + row9;
+int sum10 = line10 + row10;
+
+int sum11 = col1 + line1;
+int sum12 = col2 - line2;
+int sum13 = col3 * line3;
+int sum14 = col4 / line4;
+int sum15 = col5 + line5;
+int sum16 = col6 - line6;
+int sum17 = col7 * line7;
+int sum18 = col8 / line8;
+int sum19 = col9 + line9;
+int sum20 = col10 + line10;
+
+int sum21 = row1 + col1;
+int sum22 = row2 - col2;
+int sum23 = row3 * col3;
+int sum24 = row4 / col4;
+int sum25 = row5 + col5;
+int sum26 = row6 - col6;
+int sum27 = row7 * col7;
+int sum28 = row8 / col8;
+int sum29 = row9 + col9;
+int sum30 = row10 + col10;
+
+int end1 = 901;
+int end2 = 902;
+int end3 = 903;
+int end4 = 904;
+int end5 = 905;
+int end6 = 906;
+int end7 = 907;
+int end8 = 908;
+int end9 = 909;
+int end10 = 910;
+
+int last1 = 911;
+int last2 = 912;
+int last3 = 913;
+int last4 = 914;
+int last5 = 915;
+int last6 = 916;
+int last7 = 917;
+int last8 = 918;
+int last9 = 919;
+int last10 = 920;
+
+int final1 = 921;
+int final2 = 922;
+int final3 = 923;
+int final4 = 924;
+int final5 = 925;
+int final6 = 926;
+int final7 = 927;
+int final8 = 928;
+int final9 = 929;
+int final10 = 930;
+
+void done1() {}
+void done2() {}
+void done3() {}
+void done4() {}
+void done5() {}
+void done6() {}
+void done7() {}
+void done8() {}
+void done9() {}
+void done10() {}
+
+int out1 = end1 + last1;
+int out2 = end2 - last2;
+int out3 = end3 * last3;
+int out4 = end4 / last4;
+int out5 = end5 + last5;
+int out6 = end6 - last6;
+int out7 = end7 * last7;
+int out8 = end8 / last8;
+int out9 = end9 + last9;
+int out10 = end10 + last10;
+
+int out11 = final1 + end1;
+int out12 = final2 - end2;
+int out13 = final3 * end3;
+int out14 = final4 / end4;
+int out15 = final5 + end5;
+int out16 = final6 - end6;
+int out17 = final7 * end7;
+int out18 = final8 / end8;
+int out19 = final9 + end9;
+int out20 = final10 + end10;
+
+int out21 = last1 + final1;
+int out22 = last2 - final2;
+int out23 = last3 * final3;
+int out24 = last4 / final4;
+int out25 = last5 + final5;
+int out26 = last6 - final6;
+int out27 = last7 * final7;
+int out28 = last8 / final8;
+int out29 = last9 + final9;
+int out30 = last10 + final10;
+
+int extra1 = 1001;
+int extra2 = 1002;
+int extra3 = 1003;
+int extra4 = 1004;
+int extra5 = 1005;
+int extra6 = 1006;
+int extra7 = 1007;
+int extra8 = 1008;
+int extra9 = 1009;
+int extra10 = 1010;
+
+int add1 = extra1 + 1;
+int add2 = extra2 + 2;
+int add3 = extra3 + 3;
+int add4 = extra4 + 4;
+int add5 = extra5 + 5;
+int add6 = extra6 + 6;
+int add7 = extra7 + 7;
+int add8 = extra8 + 8;
+int add9 = extra9 + 9;
+int add10 = extra10 + 10;
+
+void more1() {}
+void more2() {}
+void more3() {}
+void more4() {}
+void more5() {}
+void more6() {}
+void more7() {}
+void more8() {}
+void more9() {}
+void more10() {}
+
+int mix1 = add1 + extra1;
+int mix2 = add2 - extra2;
+int mix3 = add3 * extra3;
+int mix4 = add4 / extra4;
+int mix5 = add5 + extra5;
+int mix6 = add6 - extra6;
+int mix7 = add7 * extra7;
+int mix8 = add8 / extra8;
+int mix9 = add9 + extra9;
+int mix10 = add10 + extra10;
+
+int fill1 = 1111;
+int fill2 = 2222;
+int fill3 = 3333;
+int fill4 = 4444;
+int fill5 = 5555;
+int fill6 = 6666;
+int fill7 = 7777;
+int fill8 = 8888;
+int fill9 = 9999;
+int fill10 = 10000;
+
+void over1() {}
+void over2() {}
+void over3() {}
+void over4() {}
+void over5() {}
+void over6() {}
+void over7() {}
+void over8() {}
+void over9() {}
+void over10() {}
+
+int all1 = fill1 + mix1;
+int all2 = fill2 - mix2;
+int all3 = fill3 * mix3;
+int all4 = fill4 / mix4;
+int all5 = fill5 + mix5;
+int all6 = fill6 - mix6;
+int all7 = fill7 * mix7;
+int all8 = fill8 / mix8;
+int all9 = fill9 + mix9;
+int all10 = fill10 + mix10;
